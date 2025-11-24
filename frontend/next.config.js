dcb867110b36c4b9445e71b73ab55285a5824325
rjkumar081 <97723829+rjkumar081@users.noreply.github.com>
2025-11-24 10:16:34 +0530

    Delete frontend directory

--- frontend/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_BACKEND: process.env.NEXT_PUBLIC_BACKEND || "http://localhost:8000"
-  }
-}
--- frontend/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:40, fontFamily:"Arial"}}>
-      <h1>PingPulse - Landing</h1>
-      <p>Simple uptime monitoring. <a href="/signup">Signup</a> or <a href="/login">Login</a>.</p>
-    </div>
-  );
-}
